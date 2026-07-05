87e200bc76c691a29dfb800842f66690e6a28e30
git-luxygalleria-ui <luxygalleria@gmail.com>
2026-07-05 17:35:02 +0530

    feat: update default WhatsApp number to 7736605422

--- dist/models/Settings.js
+++ dist/models/Settings.js
@@ -1,22 +1,22 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function (o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
+        desc = { enumerable: true, get: function () { return m[k]; } };
     }
     Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
+}) : (function (o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function (o, v) {
     Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
+}) : function (o, v) {
     o["default"] = v;
 });
 var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
+    var ownKeys = function (o) {
         ownKeys = Object.getOwnPropertyNames || function (o) {
             var ar = [];
             for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
@@ -39,7 +39,7 @@ const settingsSchema = new mongoose_1.Schema({
     bannerText: { type: String, default: '' },
     isBannerActive: { type: Boolean, default: false },
     footerText: { type: String, default: '© 2026 Genesis by Preethy. All rights reserved.' },
-    whatsappNumber: { type: String, default: '7012552969' },
+    whatsappNumber: { type: String, default: '7736605422' },
     primaryColor: { type: String, default: '#C5A866' },
     secondaryColor: { type: String, default: '#0A0A0A' },
     shippingBelow500g: { type: Number, default: 40 },
--- src/models/Settings.ts
+++ src/models/Settings.ts
@@ -17,7 +17,7 @@ const settingsSchema = new Schema<ISettings>(
     bannerText: { type: String, default: '' },
     isBannerActive: { type: Boolean, default: false },
     footerText: { type: String, default: '© 2026 Genesis by Preethy. All rights reserved.' },
-    whatsappNumber: { type: String, default: '7012552969' },
+    whatsappNumber: { type: String, default: '7736605422' },
     primaryColor: { type: String, default: '#C5A866' },
     secondaryColor: { type: String, default: '#0A0A0A' },
     shippingBelow500g: { type: Number, default: 40 },
